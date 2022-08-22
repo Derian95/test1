1b2c159c8452bffbe05f5343e3d96aa216ff8fcd
Derian <dfhamezquita@gmail.com>
2022-08-21 20:16:17 -0500

    two

--- intranet2.0/index.php
+++ intranet2.0/index.php
@@ -1,43 +1,191 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--========== BOX ICONS ==========-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
     <link rel="stylesheet" href="css/styles.css">
-    <title>Document</title><link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
+    <title>Document</title>
+    <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
     <title>Document</title>
 </head>
-<body>
-<div class="preloader" id="load"></div>
-    <nav class="sidebar_container">
-    
-        <h1>INTRAWEB RAA</h1>
-        <ul>
-            <li>
-             <a href="">Home</a>
-            </li>
-            <li>
-             <a href="">Academico</a>
-            </li>
-            <li>
-             <a href="">RESRR</a>
-            </li>
-        </ul>
 
-        <footer>
-            Cerrar Sesion
-        </footer>
-    </nav>
-    <main>
-        <header class="header">
+<body id="body">
+    <!--LOADER -->
+    <div class="pre_container" id="precon">
+        <div class="loader" id="load"></div>
+    </div>
+    <!-- HEADER-->
+    <header class="header" id="header">
+        <div class="header__container">
+
+
+            <a href="#" class="header__logo">INTRANET UPT</a>
+
             <p>Derian Francisco Herrera Amezquita</p>
-        </header>
-        <section>
-            <img src="https://ejemplos-curriculum.com/wp-content/uploads/2021/06/estudiando-1024x582.jpg" alt="" >
+
+            <div class="header__toggle">
+                <i class='bx bx-menu' id="header-toggle"></i>
+            </div>
+        </div>
+    </header>
+
+    <!--========== NAV ==========-->
+    <div class="nav" id="navbar">
+        <nav class="nav__container">
+            <div>
+                <a href="#" class="nav__link nav__logo">
+                    <span class="nav__logo-name">INTRANET</span>
+                </a>
+
+                <div class="nav__list">
+                    <div class="nav__items">
+
+
+
+                        <div class="nav__dropdown">
+                            <a href="#" class="nav__link">
+                                <i class='bx bxs-graduation nav__icon'></i>
+
+                                <span class="nav__name">Academico</span>
+                                <i class='bx bx-chevron-down nav__icon nav__dropdown-icon'></i>
+                            </a>
+
+                            <div class="nav__dropdown-collapse">
+                                <div class="nav__dropdown-content">
+                                    <a href="#" class="nav__dropdown-item">Google G Suite</a>
+                                    <a href="#" class="nav__dropdown-item">Office 365 G suite</a>
+                                    <a href="#" class="nav__dropdown-item">Convenio Microsoft</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="nav__dropdown">
+                            <a href="#" class="nav__link">
+                                <i class='bx bx-user nav__icon'></i>
+                                <span class="nav__name">Alumno</span>
+                                <i class='bx bx-chevron-down nav__icon nav__dropdown-icon'></i>
+                            </a>
+
+                            <div class="nav__dropdown-collapse">
+                                <div class="nav__dropdown-content">
+                                    <a href="#" class="nav__dropdown-item">Comprobantes Electronicos </a>
+                                    <a href="#" class="nav__dropdown-item">Seguro estudiantil</a>
+
+                                </div>
+                            </div>
+                        </div>
+                        <a href="#" class="nav__link">
+                            <i class='bx bx-book-open nav__icon'></i>
+                            <span class="nav__name">Aula Virtual</span>
+                        </a>
+                        <a href="#" class="nav__link">
+                            <i class='bx bx-globe nav__icon'></i>
+                            <span class="nav__name">GPS Alumni</span>
+                        </a>
+                        <a href="#" class="nav__link">
+                            <i class='bx bxs-book-reader nav__icon'></i>
+                            <span class="nav__name">Biblioteca</span>
+                        </a>
+                    </div>
+
+                    <div class="nav__items">
+
+                        <div class="nav__dropdown">
+                            <a href="#" class="nav__link">
+                                <i class='bx bx-bell nav__icon'></i>
+                                <span class="nav__name">Extras</span>
+                                <i class='bx bx-chevron-down nav__icon nav__dropdown-icon'></i>
+                            </a>
+
+                            <div class="nav__dropdown-collapse">
+                                <div class="nav__dropdown-content">
+                                    <a href="#" class="nav__dropdown-item">Recorrido de buses</a>
+                                    <a href="#" class="nav__dropdown-item">Reglamento y directivas</a>
+                                    <a href="#" class="nav__dropdown-item">Becas y subvenciones</a>
+
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <a href="#" class="nav__link nav__logout">
+                <i class='bx bx-log-out nav__icon'></i>
+                <span class="nav__name">Cerrar Sesion</span>
+            </a>
+        </nav>
+    </div>
+
+    <!--========== MAIN ==========-->
+    <main id="main">
+        <section class="main_header">
+            <img src="https://pro.sony/s3/2021/03/30085822/JB16-PTZ_RC-for-Webcam-app-page_banner_04.jpg" alt="text">
+            <div class="gradient">ss</div>
+
+            <div class="main_header_text">
+                <h1>BIENVENIDOS A LA INTRANET Version 1.6 (TEIN)</h1>
+                <p>
+                    <strong>¿Tienes problemas con la Intranet? </strong> <br /> entonces escríbenos a intranet@upt.pe enviando tu código universitario y datos personales.<br /><br />
+                    <strong>¿Tienes problemas con tu cuenta de Correo Institucional Live@Edu?</strong> <br /> entonces escríbenos a soporte365@upt.pe enviando tu código universitario y datos personales.<br /><br />
+                    <strong>¿Tienes problemas con tu cuenta de Google?</strong> <br /> entonces escríbenos a soportegsuite@virtual.upt.pe enviando tu código universitario y datos personales.<br /><br />
+                </p>
+            </div>
+
+        </section>
+
+
+        <section class="main_carrusel">
+            <img src="https://net.upt.edu.pe/imagenes/2022-II/CALENDARIO_ACADEMICO_2022-II.png" alt="">
+        </section>
+        <section style="text-align: center;">
+            <h2>Informacion Economica</h2>
+            <div class="table-wrapper">
+                <table class="fl-table">
+                    <thead>
+                        <tr>
+                            <th>Nro</th>
+                            <th>Descripcion</th>
+                            <th>Monto</th>
+                            <th>Vence</th>
+                            <th>Dependencia</th>
+                            <th>Semestre</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>    1</td>
+                            <td>Deuda</td>
+                            <td>584</td>
+                            <td>30/10/2022</td>
+                            <td>Dependencia</td>
+                            <td>5</td>
+                        </tr>
+                        <tr>
+                            <td>    1</td>
+                            <td>Deuda</td>
+                            <td>584</td>
+                            <td>30/10/2022</td>
+                            <td>Dependencia</td>
+                            <td>5</td>
+                        </tr>
+                       
+                       
+                    <tbody>
+                </table>
+            </div>
         </section>
     </main>
+
+    <!--========== MAIN JS ==========-->
+    <script src="assets/js/main.js"></script>
 </body>
 
-<script src="sda.js"></script>
+<script src="intranet.js"></script>
+
 </html>
